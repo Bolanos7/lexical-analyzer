ca18a2ec5352a5a120119959cb192da96eaaee1a
Cristian <cristianbolanos@yahoo.com>
2024-02-29 18:33:37 -0500

    corrected the isAlpha function

--- lexer.js
+++ lexer.js
@@ -25,18 +25,25 @@ class Lexer {
     return this.#stream[this.#cursor];
   }
 
+  //method to tokenize the input and gives the default value of input as an empty string if no input is provided
   tokenize(input = "") {
+    //initialize the stream and cursor
     this.#stream = input;
     this.#cursor = 0;
 
+    //array to store the tokens
     const tokens = [];
 
+    //loop through the stream of characters and tokenize them. We can add to this loop later to tokenize more speciafic sequences of characters
     while (this.#cursor < this.#stream.length) {
       switch (this.#at()) {
+        //first few cases before the break are to skip over white spaces
         case " ":
         case "\t":
         case "\n":
           break;
+
+        //mathematical operators
         case "+":
           tokens.push({ type: "PLUS", value: "+" });
           this.#cursor++;
@@ -53,6 +60,8 @@ class Lexer {
           tokens.push({ type: "DIVIDE", value: "/" });
           this.#cursor++;
           break;
+
+        //tokens for parentheses
         case "(":
           tokens.push({ type: "LPAREN", value: "(" });
           this.#cursor++;
@@ -61,6 +70,8 @@ class Lexer {
           tokens.push({ type: "RPAREN", value: ")" });
           this.#cursor++;
           break;
+
+        //First check if the character is a number, then check if it is a string, if neither throw an error.
 
         default:
           if (isNumeric(this.#at())) {
@@ -100,19 +111,28 @@ class Lexer {
           }
           break;
       }
+      //increment the cursor to move to the next character
       this.#cursor++;
     }
+    //outside the loop, push the EOF token to the array of tokens to indicate the end of the file and return the array of tokens
     tokens.push({ type: TokenTypes.EOF, value: "EOF" });
     return tokens;
   }
 }
 
+//Helper functions to check if a character is a number or a string
 function isNumeric(char = "") {
+  //check if the character is a number by checking its ASCII code. 0-9 are the ranges for numbers.
   return char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57;
 }
 
 function isAlpha(char = "") {
-  return /^[a-zA-Z]+$/.test(char);
+  return (
+    // Check if char is a letter by checking its ASCII code. A-Z OR!!! a-z are the ranges for letters.
+    // if either one is true, return true.
+    (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) || // Uppercase A-Z
+    (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) // Lowercase a-z
+  );
 }
 
 module.exports = { Lexer, TokenTypes };
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { Lexer } = require("./lexer");
 const lexer = new Lexer();
 
-const tokens = lexer.tokenize("10 + 45 - 3      hello world");
+const tokens = lexer.tokenize("10 + 45 - 3  hello world");
 console.log(tokens);
